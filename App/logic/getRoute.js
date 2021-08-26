633c2d61162cd2b56c757733f6524e08f4cbaa77
levizwannah <levizwannah@gmail.com>
2021-08-26 15:52:07 +0300

    finished the get Route function

--- App/logic/getRoute.js
+++ App/logic/getRoute.js
@@ -1,8 +1,16 @@
-import { api } from "../config/api";
-import React, { useContext } from "react";
-import { AppContext } from "../util/AppContext";
-import { Log } from "../util/Logger";
+import { GOOGLE_MAPS_API_KEY } from "@env";
 
 export const getRoute = async function (startPlaceId, endPlaceId) {
-    return api.post(url, {});
+    try {
+        let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?
+        origin=place_id:${startPlaceId}
+        &destination=place_id:${endPlaceId}
+        &key=${GOOGLE_MAPS_API_KEY}`);
+        let route = await resp.json();
+        return route;
+    } 
+    
+    catch(error) {
+        return error
+    }
 };
