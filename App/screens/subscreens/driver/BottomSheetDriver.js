83ae5af26a820e3ad2d06fcc75db51aee7841c9c
Tony Mogoa <mogoa.tonny@gmail.com>
2021-08-29 21:33:48 +0300

    minor tweaks

--- App/screens/subscreens/driver/BottomSheetDriver.js
+++ App/screens/subscreens/driver/BottomSheetDriver.js
@@ -1,7 +1,6 @@
 import React from "react";
 import tw from "tailwind-react-native-classnames";
-import { Dimensions } from "react-native";
-import { View, StyleSheet } from "react-native";
+import { View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import DriverStatus from "./DriverStatus";
 import RideStatus from "./RideStatus";
@@ -12,7 +11,7 @@ const BottomSheetDriver = () => {
         <View
             style={[tw`bg-white rounded-tl-lg rounded-tr-lg shadow-lg flex-1`]}
         >
-            <Stack.Navigator initialRouteName="RideStatus">
+            <Stack.Navigator initialRouteName="DriverStatus">
                 <Stack.Screen
                     name="DriverStatus"
                     component={DriverStatus}
@@ -29,15 +28,3 @@ const BottomSheetDriver = () => {
 };
 
 export default BottomSheetDriver;
-
-const styles = StyleSheet.create({
-    height_90: {
-        height: Dimensions.get("window").height * 0.9,
-    },
-});
-
-const renderHeader = () => (
-    <View style={tw`flex justify-center items-center`}>
-        <View style={tw`w-8 h-2 mt-1 rounded-full bg-gray-200`}></View>
-    </View>
-);
