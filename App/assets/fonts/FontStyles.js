1febafe634da3964040348a8b84f00d5b95da431
levizwannah <levizwannah@gmail.com>
2021-08-29 03:03:44 +0300

    minor fixes

---
+++ App/assets/fonts/FontStyles.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import fonts from "../assets/fonts/fonts";
+
+export default StyleSheet.create({
+    fp: {
+        fontFamily: fonts.poppinsRegular,
+    },
+    fpm: {
+        fontFamily: fonts.poppinsMedium,
+    },
+    fi: {
+        fontFamily: fonts.interRegular,
+    },
+    fim: {
+        fontFamily: fonts.interMedium,
+    },
+});
